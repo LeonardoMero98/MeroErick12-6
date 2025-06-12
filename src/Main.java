dce8ae8cae8217d954363def8cd9fdd65b0ed960
LeonardoMero98 <erick.mero@udla.edu.ec>
2025-06-12 17:21:02 -0500

    Codigo II terminado con un cuadrado adicional

--- src/Main.java
+++ src/Main.java
@@ -7,45 +7,26 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        // Crear círculo
-        CirculoDibujable circulo = new CirculoDibujable(80, 50, 50);
+        CirculoDibujable circulo = new CirculoDibujable(100, 0, 0);
+        CuadradoDibujable cuadrado = new CuadradoDibujable(100, 150, 50);
 
-        // Crear cuadrado
-        CuadradoDibujable cuadrado = new CuadradoDibujable(100, 200, 100);
+        System.out.println("El perimetro del circulo es: " + circulo.getPerimetro());
+        System.out.println("El area del circulo es: " + circulo.getArea());
 
-        // Mostrar información del círculo
-        System.out.println("CÍRCULO:");
-        System.out.println("El perímetro del círculo es: " + circulo.getPerimetro());
-        System.out.println("El área del círculo es: " + circulo.getArea());
+        System.out.println("El perimetro del cuadrado es: " + cuadrado.getPerimetro());
+        System.out.println("El area del cuadrado es: " + cuadrado.getArea());
 
-        // Mostrar información del cuadrado
-        System.out.println("\nCUADRADO:");
-        System.out.println("El perímetro del cuadrado es: " + cuadrado.getPerimetro());
-        System.out.println("El área del cuadrado es: " + cuadrado.getArea());
-
-        // Crear ventana
-        JFrame frame = new JFrame("Dibujar figuras geométricas");
+        JFrame frame = new JFrame("Dibujar circulo");
         JPanel panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-
-                // Dibujar círculo en rojo
-                g.setColor(Color.RED);
                 circulo.dibujar2D(g);
-
-                // Dibujar cuadrado en azul
-                g.setColor(Color.BLUE);
                 cuadrado.dibujar2D(g);
-
-                // Agregar etiquetas
-                g.setColor(Color.BLACK);
-                g.drawString("Círculo", 70, 45);
-                g.drawString("Cuadrado", 220, 95);
             }
         };
 
-        panel.setPreferredSize(new Dimension(400, 300));
+        panel.setPreferredSize(new Dimension(400, 400));
         frame.add(panel);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
